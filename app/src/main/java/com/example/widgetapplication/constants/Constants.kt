d81adae1ffb88671837d9c43a1244765d9d052c9
Gaurav Dubey <gaurav.dubey99@gmail.com>
2021-08-17 12:47:33 +0530

    Minor testing if username has changed in Github

--- app/src/main/java/com/example/widgetapplication/constants/Constants.kt
+++ app/src/main/java/com/example/widgetapplication/constants/Constants.kt
@@ -4,6 +4,7 @@ import java.util.regex.Pattern
 
 const val MINIMUM_PASSWORD_LENGTH = 8
 
+
 //Regex Pattern
 val REGEX_AT_LEAST_ONE_CAPS_ONE_SMALL_ONE_NUMBER : Pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$")
 val REGEX_AT_LEAST_ONE_SPECIAL_CHARACTER : Pattern = Pattern.compile("^(?=.*[@#\$%^&+=]).*$")
